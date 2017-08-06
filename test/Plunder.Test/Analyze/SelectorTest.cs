3a2e818e1aca5bc12696c33c19c9d91033ab7525
supernebula <supernebula@126.com>
2017-08-06 18:13:17 +0800

    Test  Selenium.PhantomJs

---
+++ test/Plunder.Test/Analyze/SelectorTest.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Diagnostics;
+
+namespace Plunder.Test.Analyze
+{
+    [TestClass]
+    public class SelectorTest
+    {
+        [TestInitialize]
+        public void Init()
+        {
+        }
+
+        [TestMethod]
+        public void ExtractTest()
+        {
+            var httpClient = new HttpClient();
+            var html = httpClient.GetStringAsync("https://movie.dobn.com/subject/26363254/comments").GetAwaiter().GetResult();
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+            var h1Node = doc.DocumentNode.SelectSingleNode("//*[@id=\"content\"]/h1");
+            var commentNodes = doc.DocumentNode.SelectNodes("//*[@id=\"comments\"]/div");
+            foreach (var node in commentNodes)
+            {
+                var avatar = node.SelectSingleNode("div[1]/a/img");
+                var nick = node.SelectSingleNode("div[2]/h3/span[2]/a");
+                var rating = node.SelectSingleNode("div[2]/h3/span[2]/span[2]");
+                var time = node.SelectSingleNode("div[2]/h3/span[2]/span[3]");
+                var agreen = node.SelectSingleNode("div[2]/h3/span[1]/span");
+                var comment = node.SelectSingleNode("div[2]/p");
+
+
+                //*[@id="comments"]/div[1]/div[2]/h3/span[2]/a
+                //*[@id="comments"]/div[2]/div[2]/h3/span[2]/a
+                //*[@id="comments"]/div[3]/div[2]/h3/span[2]/a
+                //*[@id="comments"]/div[4]/div[2]/h3/span[2]/a
+
+
+
+                if (avatar == null && nick == null)
+                    break;
+
+                var src = avatar?.GetAttributeValue("src", "");
+                Trace.WriteLine($"avatar:{src}");
+                Trace.WriteLine($"nick:{nick?.InnerText?.Trim()}");
+                Trace.WriteLine($"rating:{rating?.InnerText?.Trim()}");
+                Trace.WriteLine($"time:{time?.InnerText?.Trim()}");
+                Trace.WriteLine($"agreen:{agreen?.InnerText?.Trim()}");
+                Trace.WriteLine($"comment:{comment?.InnerText?.Trim()}");
+            }
+
+            httpClient.Dispose();
+        }
+    }
+}
---
+++ test/Plunder.Test/Download/PhantomJsTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.PhantomJS;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plunder.Test.Download
+{
+    [TestClass]
+    public class PhantomJsTest
+    {
+        [TestMethod]
+        public void PhantomJsTest1()
+        {
+            var url = "https://www.baidu.com/";
+            var driver1 = new PhantomJSDriver();
+            driver1.Navigate().GoToUrl(url);
+            var content = driver1.PageSource;
+            driver1.Quit();
+            Trace.WriteLine(content);
+        }
+
+        private PhantomJSDriverService GetPhantomJSDriverService()
+        {
+            PhantomJSDriverService pds = PhantomJSDriverService.CreateDefaultService();
+            //设置代理服务器地址
+            //pds.Proxy = $"{ip}:{port}"; 
+            //设置代理服务器认证信息
+            //pds.ProxyAuthentication = GetProxyAuthorization();
+            return pds;
+        }
+    }
+
+
+}
